35aa5299dfdc769ce7b0a25ee2402ef7d469f0e4
yashyt4980 <152067480+yashyt4980@users.noreply.github.com>
2024-02-02 21:49:58 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
       <div className="small-screen hidden">
           <img
-            src={HeroImage}
+            src=""
             alt="profile"
             className="rounded-2xl mx-auto w-2/3 md:w-full"
           />
